fd392a652610db17d3a77268e1086b404a245416
Noor Mohamed <noor.mohamed@macys.com>
2020-05-04 01:13:20 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# ScanToSave 🏩
+
+Scan QR code to add item in cart and checkout.
+
+Using [Ionic](https://ionicframework.com/docs) and [React](https://reactjs.org/) Framework.
+
+
+## Getting Started
+
+To setup in local , First install [Node.js](https://nodejs.org) , Verify the installation, by running:
+
+```bash
+node -v
+# output
+v8.11.3
+```
+
+Installation dependency using `npm` 
+
+```bash
+npm install
+```
+
+After the dependency is installed, you can start application and run this command 
+
+```bash	
+npm run start
+```
+
+Build the application using this command
+
+```bash
+npm run build
+```
+
+**NOTE:** Do revert me with creating issues or creating pull request.
+
+
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar ,IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { QRScanner ,QRScannerStatus } from '@ionic-native/qr-scanner';
+
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const openScanner = () => {  
+  QRScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+
+       // start scanning
+       let scanSub = QRScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+
+         QRScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e))};
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +44,7 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonButton onClick={openScanner}>Scan barcode</IonButton>
       </IonContent>
     </IonPage>
   );
